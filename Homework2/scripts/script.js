84e60789dce76a456cc0ea556cad9feea8d19e31
Василенко Игорь <warface.777@mail.ru>
2023-10-17 17:22:17 +0300

    Homework2

---
+++ Homework2/scripts/script.js
@@ -0,0 +1,43 @@
+new Vue({
+  el: ".blog__items",
+  data: {
+    articles: [
+      {
+        image: "blog1.jpg",
+        imgText: "Kitchan Design",
+        itemTitle: "Let’s Get Solution For Building Construction Work",
+        itemData: "26 December,2022",
+      },
+      {
+        image: "blog2.jpg",
+        imgText: "Living Design",
+        itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+        itemData: "22 December,2022",
+      },
+      {
+        image: "blog3.jpg",
+        imgText: "Interior Design",
+        itemTitle: "Best For Any Office & Business Interior Solution",
+        itemData: "25 December,2022",
+      },
+      {
+        image: "blog4.jpg",
+        imgText: "Kitchan Design",
+        itemTitle: "Let’s Get Solution For Building Construction Work",
+        itemData: "26 December,2022",
+      },
+      {
+        image: "blog5.jpg",
+        imgText: "Living Design",
+        itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+        itemData: "22 December,2022",
+      },
+      {
+        image: "blog6.jpg",
+        imgText: "Interior Design",
+        itemTitle: "Best For Any Office & Business Interior Solution",
+        itemData: "25 December,2022",
+      },
+    ],
+  },
+});
